fa35bd91c5d0ba1d0b4330c05f77d7bdd84552fb
jpsalles21 <j.salles001@edu.pucrs.br>
2025-05-06 18:39:39 -0300

    fix: post /usuarios

--- src/domains/usuario/usuario-routes.ts
+++ src/domains/usuario/usuario-routes.ts
@@ -2,6 +2,6 @@ import { FastifyInstance } from 'fastify';
 import { createUser, getAllUsers } from './usuario-controller';
 
 export default async function usuarioRotas(fastify: FastifyInstance) {
-  fastify.post('/criarUsuario', createUser);
+  fastify.post('/', createUser);
   fastify.get('/', getAllUsers);
 }
