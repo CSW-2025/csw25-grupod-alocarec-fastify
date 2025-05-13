48d80a109a2622d030e8309319937f1c6d6b0dc4
Thales Xavier <thalescaceres@gmail.com>
2025-05-13 11:20:49 -0300

    subindo migration e atualizando docker compose (teste)

---
+++ prisma/migrations/20250508213050_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "ativo" BOOLEAN NOT NULL DEFAULT true,
+    "tipo" TEXT NOT NULL DEFAULT 'usuario',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
