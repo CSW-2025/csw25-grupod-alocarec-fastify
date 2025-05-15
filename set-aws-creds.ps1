7bdf41b5b710e0f8ca84af9fd0581ec77a1ca5e0
Thales Xavier <thalescaceres@gmail.com>
2025-05-15 01:10:30 -0300

    arrumando script para atualizar variaveis aws

--- set-aws-creds.ps1
+++ set-aws-creds.ps1
@@ -1,5 +1,4 @@
-
-# Carrega as variáveis do .env.aws
+# Carrega variáveis do .env.aws
 Get-Content .env.aws | ForEach-Object {
     if ($_ -match "^(.*?)=(.*)$") {
         $name = $matches[1]
@@ -16,9 +15,11 @@ $profileContent = @"
 aws_access_key_id=$env:AWS_ACCESS_KEY_ID
 aws_secret_access_key=$env:AWS_SECRET_ACCESS_KEY
 aws_session_token=$env:AWS_SESSION_TOKEN
+
 "@
 
-$profileContent | Set-Content -Path $awsCredentialsPath -Encoding UTF8
+$utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $false
+[System.IO.File]::WriteAllText($awsCredentialsPath, $profileContent, $utf8NoBomEncoding)
 
 Write-Host "Credenciais AWS atualizadas com sucesso!"
 
